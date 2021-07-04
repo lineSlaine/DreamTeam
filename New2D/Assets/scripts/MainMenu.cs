a34374e4b00a5129d3ad5388b4d173613c5c5d42
lineSlaine <musfobbie@mail.ru>
2021-07-04 16:37:09 +0300

    Create simple menu

---
+++ New2D/Assets/scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void ExitGame()
+    {
+        Debug.Log("Game has been closed!");
+        Application.Quit();
+    }
+
+}
