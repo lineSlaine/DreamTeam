9145135d9b980299b6152fecbbe99270eb05cdec
lineSlaine <musfobbie@mail.ru>
2021-07-05 17:12:03 +0300

    Add music

---
+++ New2D/Assets/scripts/MusicButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicButton : MonoBehaviour
+{
+    public Sprite _offVolume, _onVolume;
+    Image _VolumeImage;
+    void Start()
+    {
+        _VolumeImage = GetComponent<Image>();
+        _VolumeImage.sprite = _onVolume; 
+    }
+    void Update()
+    {
+        
+    }
+    public void SwitchSpriteVolume()
+    {
+        if(_VolumeImage.sprite == _onVolume) _VolumeImage.sprite = _offVolume;
+        else _VolumeImage.sprite = _onVolume;
+    }
+}
+
